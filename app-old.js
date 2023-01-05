52a64913f5ea0f45ce02d413c60960fd3c59cb8f
Edgar Ramos Luna <edgarramoslun333@gmail.com>
2023-01-05 17:21:02 -0600

    Webserver listo

---
+++ app-old.js
@@ -0,0 +1,27 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+
+  //  console.log(req);
+  /*  res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200, {
+        'Content-Type': 'application/csv'
+    });
+
+    /*const persona = {
+        id: 1,
+        nombre: 'Edgar'
+    }
+    res.write(JSON.stringify(persona));*/
+    /*res.write('id, nombre \n');
+    res.write('1, Fernando\n');
+    res.write('2, Maria\n');
+    res.write('3, Juan\n');
+    res.write('4, Pedro\n');*/
+    res.write('Hola mundo');
+    res.end();
+})
+.listen(8080);
+
+
+console.log('Escuchando en el puerto', 8080);
---
+++ app.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+const app = express()
+const port = process.env.PORT;
+// require('hbs');
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+// Servir contenido estatico
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home', {
+    nombre: 'Edgar Ramos',
+    titulo: 'Curso de Node',
+  });
+});
+/*app.get('/hola-mundo', function (req, res) {
+    res.send('Hola Mundo')
+});*/
+
+app.get('/elements', function (req, res) {
+  //  res.sendFile(__dirname + '/public/elements.html');
+  res.render('elements', {
+    nombre: 'Edgar Ramos',
+    titulo: 'Curso de Node',
+  });
+});
+app.get('/generic', function (req, res) {
+   //res.sendFile(__dirname + '/public/generic.html');
+   res.render('generic', {
+    nombre: 'Edgar Ramos',
+    titulo: 'Curso de Node',
+   });
+});
+
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, () =>{
+    console.log(`app listening at http://localhost:${port}`);
+    
+});
